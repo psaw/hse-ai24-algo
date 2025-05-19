b993decf4d094f35e6a9b34aaef1750bf260fa50
Yury Konov <gkonoff@gmail.com>
2025-05-19 18:33:56 +0300

    A

---
+++ contest-5-mid/A/a.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+path = os.path.dirname(os.path.abspath(__file__))
+sys.stdin = open(path + "/input1.txt", "r")
+
+N, K = map(int, input().split())
+
+def solve(arr, n, k):
+    # База индукции: получили длину n - вывод на печать
+    if len(arr) == n:
+        print(' '.join(map(str, arr)))
+        return
+    
+    # Перебор значений для текущей позиции
+    for i in range(1, k + 1):
+        # Добавить элемент
+        arr.append(i)
+        # Рекурсивно достраиваем следующие
+        solve(arr, n, k)
+        # Удаляем, чтобы в следующем шаге цикла вставить очередной
+        arr.pop()
+
+solve([], N, K)
